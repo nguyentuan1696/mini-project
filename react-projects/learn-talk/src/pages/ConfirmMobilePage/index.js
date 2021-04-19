875c43382a036f02262b4a6e9c2c8b15d4dd1636
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-04-19 18:07:09 +0700

    update

--- react-projects/learn-talk/src/pages/ConfirmMobilePage/index.js
+++ react-projects/learn-talk/src/pages/ConfirmMobilePage/index.js
@@ -17,9 +17,22 @@ const ConfirmMobilePage = () => {
   const [isLogin, , setLogin] = useState('')
   const [popUp, setPopUp] = useState(false)
 
-      let tmp_mobile =
-        fromProps === 'index' || fromProps === 'register' ? accProps : ''
+  const [code1, setCode1] = useState('')
+  const [code2, setCode2] = useState('')
+  const [code3, setCode3] = useState('')
+  const [code4, setCode4] = useState('')
+  const [code5, setCode5] = useState('')
+  const [code6, setCode6] = useState('')
 
+  const inputE1 = useRef(null)
+  const inputE2 = useRef(null)
+  const inputE3 = useRef(null)
+  const inputE4 = useRef(null)
+  const inputE5 = useRef(null)
+  const inputE6 = useRef(null)
+
+  let tmp_mobile =
+    fromProps === 'index' || fromProps === 'register' ? accProps : ''
 
   const hideKeyBoard = () => {
     let agent = window.navigator.userAgent
@@ -27,6 +40,29 @@ const ConfirmMobilePage = () => {
       window.KingIDSdk.onHideKeyBoard()
       window.ee.removeListener('show_keyboard')
     }
+  }
+
+  const handleCode1 = () => {}
+
+  const handleKeyUp = () => { }
+  
+  const showInputOTP = () =>
+  {
+    
+  }
+
+  
+
+  const onPaste = (e) => {
+    let text = e.clipboardData.getData('text')
+    setCode1(text.slice(0, 1))
+    setCode1(text.slice(0, 1))
+    setCode2(text.slice(1, 2))
+    setCode3(text.slice(2, 3))
+    setCode4(text.slice(3, 4))
+    setCode5(text.slice(4, 5))
+    setCode6(text.slice(5, 6))
+    inputE6.current.focus()
   }
 
   let dataState = {
@@ -105,31 +141,35 @@ const ConfirmMobilePage = () => {
         </Link>
       )
     }
-    
-    
-      back = (
-        <Link
-          className='img-back'
-          to={{ pathname: '/talk/index' }}
-          onClick={() => hideKeyBoard()}
-        >
-          <img src='https://mingid.mediacdn.vn/king/image/back.png' alt='' />
-        </Link>
-      )
-    
 
+    back = (
+      <Link
+        className='img-back'
+        to={{ pathname: '/talk/index' }}
+        onClick={() => hideKeyBoard()}
+      >
+        <img src='https://mingid.mediacdn.vn/king/image/back.png' alt='' />
+      </Link>
+    )
 
     let classImg =
       fromProps === 'register' || isVietID ? (
         <div className='img-logo marginT64'>
-          <img src='https://mingid.mediacdn.vn/king/image/logo-vietid.png' alt=''></img>
+          <img
+            src='https://mingid.mediacdn.vn/king/image/logo-vietid.png'
+            alt=''
+          ></img>
         </div>
       ) : (
         ''
       )
-    
-    let classTitle = (fromProps === 'register' || isVietID) ? 'title marginT24' : 'title'
-    let className = (popUp && (fromProps === 'addMobile' || fromProps === 'updateMobileApple')) ? 'modal-wrapper' : 'modal-wrapper display-none'
+
+    let classTitle =
+      fromProps === 'register' || isVietID ? 'title marginT24' : 'title'
+    let className =
+      popUp && (fromProps === 'addMobile' || fromProps === 'updateMobileApple')
+        ? 'modal-wrapper'
+        : 'modal-wrapper display-none'
 
     return (
       <div className='wrapper-enter-otp'>
@@ -139,6 +179,10 @@ const ConfirmMobilePage = () => {
         <div className={classTitle}>
           Nhập mã OTP được gửi về Số điện thoại
           <span>{tmp_mobile ? tmp_mobile : mobileProps}</span>
+        </div>
+
+        <div className='wrapper-input'>
+          <div className='item'>{showInputOTP}</div>
         </div>
       </div>
     )
